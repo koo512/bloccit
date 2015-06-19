4d7cd68efb1eef2ca72e938e85334a4a2632c1dc
koo512 <koo512@gmail.com>
2015-06-19 18:04:01 -0400

    Assignment 39 ScopeSubclass, Complete

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -3,5 +3,17 @@ class PostPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  class Scope < Scope
+
+    def resolve
+      if user.present? && (user.admin? || user.moderator?)
+        scope.all
+      elsif user.present?
+        scope.where(:user_id => user)
+      else scope.none
+      end
+    end
+  end
   
 end
