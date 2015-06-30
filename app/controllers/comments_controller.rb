14abc95059de2b539b6fb00f82390f2ab1ef0351
koo512 <koo512@gmail.com>
2015-06-30 13:25:24 -0400

    Assignment 47 Implement Shallow Routing, Complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
 
   def create
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id]) 
     @comment = current_user.comments.build(comment_params)
     authorize @comment
@@ -9,25 +8,24 @@ class CommentsController < ApplicationController
     authorize @comment
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
  
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 
